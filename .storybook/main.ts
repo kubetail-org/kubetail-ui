ec8225f55805bd51e1c5a8c5a65bbce3461a9854
Max Rukomoynikov <cat@mos-it.com>
2025-06-06 17:31:51 +0100

    chore: update storybook to v9 (#31)
    * Update .gitignore to exclude (p)npm packages dir
    * Update storybook version and components to use updated version
    * Install @vueless/storybook-dark-mode to support dark mode and update react related npm packages to support new Storybook
    * Dev environment in docker
    
    ---------
    
    Signed-off-by: Max Rukomoynikov <rukomoynikov@gmail.com>

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,21 +2,20 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
-    "@storybook/addon-essentials",
     "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
     "@storybook/addon-themes",
-    'storybook-dark-mode',
+    "@storybook/addon-docs",
+    '@vueless/storybook-dark-mode'
   ],
-  framework: {
-    name: "@storybook/react-vite",
-    options: {},
-  },
-  docs: {
-    autodocs: "tag",
-  },
+
+  framework: "@storybook/react-vite",
+
+  core: {
+    builder: '@storybook/builder-vite',
+  }
 };
 export default config;
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,4 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from "@storybook/react-vite";
 import * as colors from 'tailwindcss/colors';
 
 import '../tailwind.css';
