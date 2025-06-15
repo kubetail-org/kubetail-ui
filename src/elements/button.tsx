237c636fa2bd6b6729932c051c9bf357a413c1f9
Andres Morey <andresmarcel@gmail.com>
2025-06-15 13:03:48 +0300

    wip

--- src/elements/button.tsx
+++ src/elements/button.tsx
@@ -5,11 +5,11 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:bg-bg-disabled disabled:text-text-disabled [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all active:ring-2 active:ring-offset-2 active:ring-border-focusRing disabled:pointer-events-none disabled:bg-bg-disabled disabled:text-text-disabled [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
-        primary: 'bg-bg-primary text-text-on-color shadow-xs hover:bg-bg-primary-hover',
+        primary: 'bg-bg-primary text-text-on-color shadow-xs hover:bg-bg-primary-hover active:bg-bg-primary-active',
         secondary: 'bg-bg-secondary text-text shadow-xs hover:bg-bg-secondary-hover',
         outline: 'border border-border-input bg-bg shadow-xs hover:bg-bg-elevated disabled:bg-bg',
         destructive:
