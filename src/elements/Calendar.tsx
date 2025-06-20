d12fe60ea1b0c21f7e95f54d29290284e9b01483
Andres Morey <andresmarcel@gmail.com>
2025-06-20 13:29:34 +0300

    wip

--- src/elements/Calendar.tsx
+++ src/elements/Calendar.tsx
@@ -1,3 +1,4 @@
+/*
 'use client';
 
 import { ChevronLeft, ChevronRight } from 'lucide-react';
@@ -62,3 +63,35 @@ function Calendar({
 Calendar.displayName = 'Calendar';
 
 export { Calendar };
+*/
+
+import { DayPicker, getDefaultClassNames } from "react-day-picker";
+
+export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+
+export function Calendar({
+    showOutsideDays = true,
+  ...props
+}) {
+  const defaultClassNames = getDefaultClassNames();
+  return (
+    <DayPicker
+      mode="single"
+      showOutsideDays={showOutsideDays}
+      classNames={{
+        today: 'bg-accent text-accent-foreground',
+        selected: 'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
+        root: `${defaultClassNames.root} shadow-lg p-5`,
+        chevron: `${defaultClassNames.chevron} fill-amber-500`,
+        day_range_end: 'day-range-end',
+        day_selected: 'bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground',
+        day_today: 'bg-accent text-accent-foreground',
+        day_outside: 'day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30',
+        day_disabled: 'text-muted-foreground opacity-50',
+        day_range_middle: 'aria-selected:bg-accent aria-selected:text-accent-foreground',
+        day_hidden: 'invisible',
+      }}
+      {...props}
+    />
+  );
+}
