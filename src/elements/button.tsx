ba2ae2ce9e8b5451a69cb00b429febc8a650d31e
Andres Morey <andresmarcel@gmail.com>
2025-07-18 10:57:06 +0300

    new: catch up with figma and other bug fixes
    * Moved button focus ring to focus-visible
    * Updated color tokens to match figma
    * Added missing @source directive to tailwind.css
    
    Signed-off-by: Andres Morey <andresmarcel@gmail.com>

--- src/elements/button.tsx
+++ src/elements/button.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-  "appearance-none inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all ring-0 ring-transparent ring-offset-2 active:ring-2 active:ring-border-focusRing disabled:pointer-events-none disabled:bg-bg-disabled disabled:text-text-disabled disabled:shadow-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+  "appearance-none inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all ring-0 ring-transparent ring-offset-2 disabled:pointer-events-none disabled:bg-bg-disabled disabled:text-text-disabled disabled:shadow-none [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:ring-2 focus-visible:ring-border-focusRing aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
   {
     variants: {
       variant: {
