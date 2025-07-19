9ae9948665a3f7f527311d2a91b39fe204f9140f
Andres Morey <andresmarcel@gmail.com>
2025-07-19 21:55:23 +0300

    wip

---
+++ src/elements/dropdown-menu.stories.tsx
@@ -0,0 +1,54 @@
+import type { DropdownMenuCheckboxItemProps } from '@radix-ui/react-dropdown-menu';
+import type { Meta, StoryObj } from '@storybook/react-vite';
+import { useState } from 'react';
+
+import { Button } from '@/elements/button';
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/elements/dropdown-menu';
+
+const meta = {
+  title: 'Elements/Dropdown Menu',
+  tags: ['autodocs'],
+  component: DropdownMenu,
+} satisfies Meta<typeof DropdownMenu>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+type Checked = DropdownMenuCheckboxItemProps['checked'];
+
+export const CheckboxesExample: Story = {
+  render: () => {
+    const [showStatusBar, setShowStatusBar] = useState<Checked>(true);
+    const [showActivityBar, setShowActivityBar] = useState<Checked>(false);
+    const [showPanel, setShowPanel] = useState<Checked>(false);
+
+    return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="outline">Open</Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56">
+          <DropdownMenuLabel>Appearance</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuCheckboxItem checked={showStatusBar} onCheckedChange={setShowStatusBar}>
+            Status Bar
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={showActivityBar} onCheckedChange={setShowActivityBar} disabled>
+            Activity Bar
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={showPanel} onCheckedChange={setShowPanel}>
+            Panel
+          </DropdownMenuCheckboxItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+  },
+};
