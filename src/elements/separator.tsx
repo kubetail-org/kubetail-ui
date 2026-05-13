0e3f516d791c6c640f3ab8d7ced542e5044b845a
Andres Morey <andres@kubetail.com>
2026-05-13 17:44:58 +0300

    refactor(separator): simplify variant classes and props type
    Signed-off-by: Andres Morey <andres@kubetail.com>

--- src/elements/separator.tsx
+++ src/elements/separator.tsx
@@ -4,17 +4,13 @@ import { Separator as SeparatorPrimitive } from '@base-ui/react/separator';
 
 import { cn } from '@/lib/utils';
 
-function Separator({
-  className,
-  orientation = 'horizontal',
-  ...props
-}: React.ComponentProps<typeof SeparatorPrimitive>) {
+function Separator({ className, orientation = 'horizontal', ...props }: SeparatorPrimitive.Props) {
   return (
     <SeparatorPrimitive
       data-slot="separator"
       orientation={orientation}
       className={cn(
-        'bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px',
+        'shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch',
         className,
       )}
       {...props}
